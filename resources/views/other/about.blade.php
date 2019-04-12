5bfdfe58b7fd26a7ac8718e5dc7bf9b849c9c7b4
ulug94 <uluginha@gamil.com>
2019-04-12 19:39:41 +0500

    Done

---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About me</p>
+        </div>
+    </div>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A architecto assumenda consequatur enim est eveniet hic, id itaque nisi, nobis quas quis saepe tempore velit voluptatibus? Blanditiis quisquam reiciendis sit.
+    </p>
+@endsection
